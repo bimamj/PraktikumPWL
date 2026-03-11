72574c0a14dd47866d9f63714251681fbc492b87
M Bima J <bimamj7@gmail.com>
2026-03-11 11:03:53 +0700

    first commit

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(),
+                TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->minLength(6)
+            ]);
+    }
+}
